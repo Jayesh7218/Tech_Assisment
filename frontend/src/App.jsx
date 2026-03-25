c0cb883770d778bae25e1d1099881e06a6ed484f
Jayesh7218 <jpatil7218@gmail.com>
2026-03-25 12:03:54 +0530

    backend changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import axios from 'axios';
 import { Play, Save, Loader2, Sparkles, BrainCircuit, Bot, Database, Zap } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 
-const API = 'https://tech-assisment-2.onrender.com/api';
+const API = 'https://tech-assisment-gtwj.vercel.app/api';
 
 function InputNode({ data }) {
   return (
